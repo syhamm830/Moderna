df238b9744257e250dd6963da413f7168bc7a1ab
syhamm830 <syrine.hammami@ensi-uma.tn>
2024-05-22 20:18:13 +0100

    7afalet

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import Contact from './pages/Contact/Contact';
-import Product from './pages/Product/Product'; // Corrected the path
+import Product from './pages/Product/Product';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
         <Route path="/product" element={<Product />} />
       </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import './Navbar.css';
+import { Link } from "react-router-dom";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
-export default Navbar;
+export default Navbar
