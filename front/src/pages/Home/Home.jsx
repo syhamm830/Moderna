029682d28d9c720a9fdb46369e2646e0f08c22aa
syhamm830 <syrine.hammami@ensi-uma.tn>
2024-05-23 09:49:40 +0100

    main

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import './Home.css';
 import main from './main.jpg'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='mainp'>
-      <img src={main} />
-    </div>
+      <>
+      <div className="topm">
+        <p>New here? Get 20% off everything! with code : ImNew</p>
+      </div>
+      <div className='mainp'>
+     
+        <img src={main} />
+        <div className="button-container">
+          <Link to="/Women"><button>SHOP WOMEN</button></Link>
+          <Link to="/Men"><button>SHOP MEN</button></Link> 
+        </div>
+      </div>
+      </>
   );
 }
 
