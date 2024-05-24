29abcf7333cb7d3456103b195f8ac163d34b5715
klaoza <155117429+klaoza@users.noreply.github.com>
2024-05-24 17:40:13 +0100

    comm

--- back/models/users.js
+++ back/models/users.js
@@ -1,32 +1,32 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   },
-//   phone: {
-//     type: String,
-//     required: true
-//   },
-//   birthdate: {
-//     type: Date,
-//     required: true
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 
 module.exports = mongoose.model('User', userSchema);;
